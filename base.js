f57e4499e1f9ee2c9232cfd839d355f27b6c2fa2
philcon93 <phil@neto.com.au>
2017-06-05 13:14:33 +1000

    updated regex for more functionality

--- base.js
+++ base.js
@@ -3,63 +3,72 @@
  *
  * @author Phil Connah
  */
-Rainbow.extend('base', [
+Rainbow.extend('html', [
 	{
 		// Opening/Closing function tags
         matches: {
-            1: 'support.tag.open',
-            2: 'support.tag.close'
+            1: 'function.tag.open',
+            2: 'function.tag.close'
         },
-        pattern: /(\[&percnt;)|(\/?\??&percnt;])/g
+        pattern: /(\[&percnt;|\[%)|(\/?\??&percnt;]|\/?\??%])/g
     },
 	{
 		// First word in function
         matches: {
-            1: 'support.tag',
-            2: 'support.tag.special',
-            3: 'support.tag-name'
+            1: 'function.tag',
+            2: 'function.tag-name'
         },
-        pattern: /(\[&percnt;\??)(\/|\!?)(\w+)/g
+        pattern: /(\[&percnt;\??|\[%\??)(\w+)/gi
     },
 	{
-		// Functions attributes keys
+		// Closing function
+		matches: {
+            1: 'function.tag.open',
+            2: 'function.tag.tag-name',
+			3: 'function.tag.close'
+        },
+        pattern: /(\[&percnt;\/|\[%\/)(\w+)(\/?\??&percnt;]|\/?\??%])/gi
+	},
+	{
+		// Functions parameters attributes keys
         matches: {
-            1: 'support.attribute.key'
+            1: 'function.parameter.key',
+			2: 'function.parameter.operator'
         },
         pattern: /([a-z_]+)(?:\:)/gi
     },
 	{
 		// Functions attributes values
         matches: {
-            1: 'support.operator',
-            2: 'string.quote',
-            3: 'string.value',
-            4: 'string.quote'
+            1: 'string.quote',
+            2: 'function.parameter.value',
+			3: 'string.quote'
         },
-        pattern: /(:)(\&#39;|")(.*?)(\2)/g
+        pattern: /(\:\&#39;|"|')([a-zA-Z0-9_-]+)(\&#39;|"|')/g
     },
 	{
-		// Opening/Closing data tags
+		// Functions * parameters attributes
         matches: {
-            1: 'support.tag.open',
-            2: 'support.tag.close'
+            1: 'function.tag.open',
+			2: 'function.tag-name',
+			3: 'function.parameter.star',
+			4: 'function.parameter.key',
+			5: 'function.tag.close'
         },
-        pattern: /(\[&#64;)|(\/?\??&#64;])/g
+        pattern: /(\[&percnt;|\[%)(\param )(\*|)([a-z_]+)(\/?\??&percnt;]|\/?\??%])/gi
     },
 	{
 		// Data tag
-        name: 'support.tag',
         matches: {
-            1: 'support.tag',
-            2: 'support.tag.special',
-			3: 'support.tag.special',
-            4: 'support.tag-name'
+            1: 'data.tag.open',
+			2: 'data.tag.value',
+            3: 'data.tag.close'
         },
-        pattern: /(\[&#64;\??)(\/|\!?)(\w+)(&#64;\])/g
-    }
+        pattern: /(\[&#64;\??|\[@\??)(\w+)(&#64;\]|@\])/g
+    },
 ]);
 
-Rainbow.addAlias('neto', 'base');
+Rainbow.addAlias('base', 'html');
 Rainbow.onHighlight(function(block, language) {
     console.log(block, 'for language', language, 'was highlighted');
 });
